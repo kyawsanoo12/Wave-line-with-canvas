7987c63aebb92105c02b114b88640d19d8715cad
kyawsanoo <skyaw6736@gmail.com>
2022-06-18 16:32:55 +0630

    add

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,4 @@
-import utils from './utils'
+import * as dat from "dat.gui";
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
@@ -11,63 +11,63 @@ const mouse = {
   y: innerHeight / 2
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+const gui = new dat.GUI();
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
-
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
-
-  init()
-})
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
-  }
+const wave = {
+  y: canvas.height / 2,
+  length: 0.01,
+  amplitude: 100,
+  frequency:0.01
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
+const waveFolder = gui.addFolder("wave");
+waveFolder.add(wave, "y", 0, canvas.height);
+waveFolder.add(wave, "length", -0.01, 0.01);
+waveFolder.add(wave, "amplitude", -300, 300);
+waveFolder.add(wave, "frequency", -0.01, 1);
+waveFolder.open();
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
+const strokeColor = {
+  h: 200,
+  s:50,
+  l: 50, 
 }
 
-// Animation Loop
+const backgroundColor = {
+  r: 0,
+  g: 0,
+  b: 0,
+  a:0.01
+}
+
+const strokeFolder = gui.addFolder("stroke color");
+strokeFolder.add(strokeColor, "h", 0, 255);
+strokeFolder.add(strokeColor, "s", 0, 100);
+strokeFolder.add(strokeColor, "l", 0, 100);
+strokeFolder.open();
+
+const backgroundFolder = gui.addFolder("background");
+backgroundFolder.add(backgroundColor, "r", 0, 255);
+backgroundFolder.add(backgroundColor, "g", 0, 255);
+backgroundFolder.add(backgroundColor, "b", 0, 255);
+backgroundFolder.add(backgroundColor, "a", 0, 1);
+backgroundFolder.open();
+
+let increment = wave.frequency;
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
-
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  requestAnimationFrame(animate);
+  c.fillStyle = `rgba(${backgroundColor.r},${backgroundColor.g},${backgroundColor.b},${backgroundColor.a})`
+  c.fillRect(0,0,canvas.width,canvas.height)
+  c.beginPath();
+c.moveTo(0, canvas.height / 2);
+for (let i = 0; i < canvas.width; i++){
+  c.lineTo(i,wave.y + Math.sin(i * wave.length + increment) * wave.amplitude * Math.sin(increment));
 }
 
-init()
+  increment += wave.frequency;
+  c.strokeStyle = `hsl(${strokeColor.h },${strokeColor.s}%,${strokeColor.l}%)`;
+c.stroke()
+
+}
+
 animate()
